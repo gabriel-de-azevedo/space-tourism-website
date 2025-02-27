82a7c626cd72ad3db38431e835f4ff0a5c228dc7
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2025-02-27 11:26:09 -0300

    feat: add TabsMenu

---
+++ src/components/TabsMenu.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex underlined-menu" style="--gap: 2rem">
+    <button
+      aria-selected="true"
+      class="uppercase ff-sans-cond text-accent bg-dark letter-spacing-2"
+    >
+      Moon
+    </button>
+    <button class="uppercase ff-sans-cond text-accent bg-dark letter-spacing-2">Mars</button>
+    <button class="uppercase ff-sans-cond text-accent bg-dark letter-spacing-2">Europa</button>
+  </div>
+</template>
