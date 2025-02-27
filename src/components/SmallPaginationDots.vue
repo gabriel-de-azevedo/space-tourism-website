8e49b5c4fab1aebf5d00fec818843c2882555416
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2025-02-27 11:26:22 -0300

    feat: add SmalPagination

---
+++ src/components/SmallPaginationDots.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex">
+    <button aria-selected="true">
+      <span class="sr-only">Tab title</span>
+    </button>
+    <button>
+      <span class="sr-only">Tab title</span>
+    </button>
+    <button>
+      <span class="sr-only">Tab title</span>
+    </button>
+  </div>
+</template>
+
+<style scoped>
+div > * {
+  cursor: pointer;
+  padding: 0.5em;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  border: 0;
+  background-color: rgb(var(--clr-white) / 0.25);
+  transition: var(--transition-short);
+}
+
+div > *:hover,
+div > *:focus {
+  background-color: rgb(var(--clr-white) / 0.5);
+}
+
+div > [aria-selected='true'] {
+  background-color: rgb(var(--clr-white) / 1);
+}
+</style>
