f31ca5b29da14e368ef29428446464922c264286
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2025-02-27 13:51:24 -0300

    build: move atoms to atoms folder

---
+++ src/components/atoms/NavigationMenu.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav>
+    <ul class="flex underlined-menu" style="--gap: 8rem">
+      <li class="active">
+        <a class="ff-sans-cond uppercase text-white letter-spacing-2" href="/"
+          ><span>00</span>Home</a
+        >
+      </li>
+      <li>
+        <a class="ff-sans-cond uppercase text-white letter-spacing-2" href="/destination"
+          ><span>01</span>Destination</a
+        >
+      </li>
+      <li>
+        <a class="ff-sans-cond uppercase text-white letter-spacing-2" href="/crew"
+          ><span>02</span>Crew</a
+        >
+      </li>
+      <li>
+        <a class="ff-sans-cond uppercase text-white letter-spacing-2" href="/technology"
+          ><span>03</span>Technology</a
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+ul a {
+  text-decoration: none;
+}
+
+ul a > span {
+  font-weight: 700;
+  margin-right: 0.5em;
+}
+</style>
