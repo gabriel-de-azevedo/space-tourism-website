7475089861ca0e376a37a60e12d34f38c5ac1414
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2025-02-27 11:25:58 -0300

    feat: add NavigationMenu

---
+++ src/components/NavigationMenu.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <ul class="flex underlined-menu" style="--gap: 8rem">
+      <li class="active">
+        <a class="uppercase text-white letter-spacing-2" href="#"><span>00</span>Active</a>
+      </li>
+      <li>
+        <a class="uppercase text-white letter-spacing-2" href="#"><span>01</span>Hovered</a>
+      </li>
+      <li>
+        <a class="uppercase text-white letter-spacing-2" href="#"><span>02</span>Idle</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+ul a {
+  text-decoration: none;
+}
+
+ul a > span {
+  font-weight: 700;
+  margin-right: 0.5em;
+}
+</style>
