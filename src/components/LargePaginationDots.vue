d3ae948f67890364c02a62d2d90351948f004e03
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2025-02-27 11:26:33 -0300

    feat: add LargePagination

---
+++ src/components/LargePaginationDots.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex">
+    <button aria-selected="true" class="bg-dark text-white fs-500 ff-serif">1</button>
+    <button class="bg-dark text-white fs-500 ff-serif">2</button>
+    <button class="bg-dark text-white fs-500 ff-serif">3</button>
+  </div>
+</template>
+
+<style scoped>
+div > * {
+  cursor: pointer;
+  min-height: 2.5em;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  border: 1px solid rgb(var(--clr-white) / 0.25);
+  transition: var(--transition-short);
+}
+
+div > *:hover,
+div > *:focus {
+  border-color: rgb(var(--clr-white));
+}
+
+div > [aria-selected='true'] {
+  background-color: rgb(var(--clr-white));
+  color: rgb(var(--clr-dark));
+}
+</style>
